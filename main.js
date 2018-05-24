23ee4735f9d9116833120321b94b1c800c38d9d9
jb <jasontbrown99@gmail.com>
2018-05-24 15:23:05 -0700

    add slide in posts; minor refactoring

--- main.js
+++ main.js
@@ -2,23 +2,34 @@ console.log('loaded');
 $(window).scroll(function() {
 
   var windowScroll = $(this).scrollTop();
-
+  var windowHeight = $(window).height();
+  var clothesPics = $('.clothes-pics');
+  var logo = $('.logo');
+  var backBird = $('.back-bird');
+  var foreBird = $('.fore-bird');
+  var largeWindow = $('.large-window');
+  // offset el.getBoundingClientRect()
+//   var rect = el.getBoundingClientRect();
+//
+// {
+//   top: rect.top + document.body.scrollTop,
+// }
   // Make logo scroll speed the fulcrum between the foreground image and the background image
-  $('.logo').css({
+  logo.css({
     'transform' : 'translate(0px, '+ windowScroll /2 +'%)'
   });
   // The background image will move the slowest
-  $('.back-bird').css({
+  backBird.css({
     'transform' : 'translate(0px, '+ windowScroll /4 +'%)'
   });
   // foreground image moves up, instead of down, and gets a negative property
   // Anything less than 40 makes the image scroll up really fast
-  $('.fore-bird').css({
+  foreBird.css({
     'transform' : 'translate(0px, -'+ windowScroll /40 +'%)'
   });
 
   // If the clothing pictures reach 20% (or window.height / 1.2) into the viewport
-  if (windowScroll > $('.clothes-pics').offset().top - $(window).height() / 1.2) {
+  if (windowScroll > clothesPics.offset().top - windowHeight / 1.2) {
     // Add a class of is-showing to each picture
     $('.clothes-pics figure').each(function(i) {
       setTimeout(function() {
@@ -27,14 +38,23 @@ $(window).scroll(function() {
     });
   }
 
-  if(windowScroll > $('.large-window').offset().top - $(window).height()) {
+  if(windowScroll > clothesPics.offset().top - windowHeight) {
     // This makes sure the model's face is at the top of the circle when its at the top of the page.
-    $('large-window').css({
-      'background-position': 'center ' + (windowScroll - $('.large-window').offset().top) + 'px'
+    largeWindow.css({
+      'background-position': 'center ' + (windowScroll - largeWindow.offset().top) + 'px'
     })
     // Fade in promotial offer
-    var opacity = (windowScroll - $('.large-window').offset().top + 400) / (windowScroll / 4.5);
+    var opacity = (windowScroll - largeWindow.offset().top + 300) / (windowScroll / 4.5);
 
     $('.window-tint').css({ 'opacity': opacity })
+  }
+
+  if(windowScroll > $('.blog-posts').offset().top - windowHeight) {
+    // This moves the blog posts according to scroll
+    var offset = Math.min(0, windowScroll - $('.blog-posts').offset().top + windowHeight - 450);
+
+    $('.post-1').css({ 'transform' : 'translate(' + offset + 'px, '+ Math.abs(offset * 0.2) + 'px)'});
+
+    $('.post-3').css({ 'transform' : 'translate(' + Math.abs(offset) + 'px, '+ Math.abs(offset * 0.2) + 'px)'});
   }
 });
