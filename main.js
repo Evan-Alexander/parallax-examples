ee87f10077cf146d4747166967ac11b58f9bf447
jb <jasontbrown99@gmail.com>
2018-05-24 10:10:34 -0700

    add fade in effect for periscope offer/cta

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $(window).scroll(function() {
     'transform' : 'translate(0px, -'+ windowScroll /40 +'%)'
   });
 
-  // If the clothing pictures reach 20% (or window.height / 1.2) into the viewport 
+  // If the clothing pictures reach 20% (or window.height / 1.2) into the viewport
   if (windowScroll > $('.clothes-pics').offset().top - $(window).height() / 1.2) {
     // Add a class of is-showing to each picture
     $('.clothes-pics figure').each(function(i) {
@@ -25,6 +25,16 @@ $(window).scroll(function() {
         $('.clothes-pics figure').eq(i).addClass('is-showing');
       }, 150 * (i + 1));
     });
+  }
 
+  if(windowScroll > $('.large-window').offset().top - $(window).height()) {
+    // This makes sure the model's face is at the top of the circle when its at the top of the page.
+    $('large-window').css({
+      'background-position': 'center ' + (windowScroll - $('.large-window').offset().top) + 'px'
+    })
+    // Fade in promotial offer
+    var opacity = (windowScroll - $('.large-window').offset().top + 400) / (windowScroll / 4.5);
+
+    $('.window-tint').css({ 'opacity': opacity })
   }
 });
